a0db70f74bb094b99f9539eafa402d4553671413
Abderrahmane <tiamani3939@gmail.com>
2023-03-01 10:13:36 +0100

    feautures items

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -1,31 +1,41 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-
-
 function Landing(props) {
- const [collections, setCollections] = useState([]);
+  const [collections, setCollections] = useState([]);
 
-    async function getCollections() {
-      const { data } = await axios.get(
-        "https://fakestoreapi.com/products"
-      );
-      setCollections(data);
-    }
-    useEffect(() => {
-      getCollections();
-    }, []);
+  async function getCollections() {
+    const { data } = await axios.get("https://fakestoreapi.com/products");
+    setCollections(data);
+  }
+  useEffect(() => {
+    getCollections();
+  }, []);
 
   return (
-    <div className="wrapper">
-      {collections.map((collection) => (
-      <div className="row" key={collection.id}>
-        <figure >
-          <img src={collection.image} alt=""  />
-        </figure>
+    <section>
+      <div className="Feautured__sec-wrapper">
+        <div className="row">
+        <h1 className="section__title">Feautured <span className="orange">Items</span></h1>
+        <div className="items__wrapper">
+        {collections
+          .sort((a, b) => b.price - a.price)
+          .slice(0, 4)
+          .map((collection) => (
+            <div className="item__info" key={collection.id}>
+              <figure>
+                <img src={collection.image} alt="" />
+              </figure>
+              <h3 className="items__title">{collection.title}</h3>
+              <p className="items__info">{collection.category}</p>
+              <p className="items__info">${collection.price}</p>
+              </div>
+            
+          ))}
+          </div>
+        </div>
       </div>
-      ))}
-    </div>
+    </section>
   );
 }
 
